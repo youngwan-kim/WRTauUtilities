811f5b53cccc1931c93bdf4f8769be99b42c5f16
Youngwan Kim <youngwan.kim@cern.ch>
2023-11-23 17:31:28 +0900

    skim option added for hadd script

--- Scripts/hadd.py
+++ Scripts/hadd.py
@@ -11,6 +11,15 @@ samplegroup = {
     "QCD" : [],
 }
 
+
+samplegroup_skim = {
+    "VVV" : ["WWW","WWZ","WZZ","ZZZ"],
+    "VV" : ["WW_pythia","WZ_pythia","ZZ_pythia"],
+    "ST" : ["SingleTop*"],
+    "TT" : ["TT*"],
+    "QCD" : ["QCD*"],
+}
+
 l_lep = ['NonpromptLepton','PromptLepton']
 l_tau = ['NonpromptTau','PromptTau']
 
@@ -23,8 +32,10 @@ def GetTMPDir() :
 def GetSKOutDir(analyzername,era) : 
     return f"/data9/Users/youngwan/SKFlatOutput/Run2UltraLegacy_v3/{analyzername}/{era}"
 
-def HADDnGet(analyzername,era,flag,outdir) :
+def HADDnGet(analyzername,era,flag,outdir,skim) :
     
+    hasSkim = skim is not ''
+
     if not flag == '' : 
         flagstr = f"{flag}__"
         outdir = f"{outdir}__{flag}"
@@ -35,7 +46,11 @@ def HADDnGet(analyzername,era,flag,outdir) :
 
     hadddir = f"{GetSKOutDir(analyzername,era)}/{flagstr}PromptLepton__PromptTau__"
     # Data
-    os.system(f"hadd ../RootFiles/{outdir}/DATA/{analyzername}_DATA.root {hadddir}/DATA/{analyzername}_Tau*")
+    basename = analyzername
+    if hasSkim : 
+        analyzername = basename + "_SkimTree_" + skim
+        samplegroup = samplegroup_skim
+    os.system(f"hadd ../RootFiles/{outdir}/DATA/{analyzername}_DATA.root {GetSKOutDir(basename,era)}/{flagstr}/DATA/{analyzername}_Tau*")
 
     # Prompt HADD
     for sample in samplegroup :
@@ -43,26 +58,27 @@ def HADDnGet(analyzername,era,flag,outdir) :
             haddstr = ""
             for name in samplegroup[sample] :
                 haddstr += f"{hadddir}/{analyzername}_{name}.root "
-            os.system(f"hadd ../RootFiles/{outdir}/{analyzername}_{sample}.root {haddstr}")
+            os.system(f"hadd ../RootFiles/{outdir}/{basename}_{sample}.root {haddstr}")
 
         else : 
-            os.system(f"hadd ../RootFiles/{outdir}/{analyzername}_{sample}.root {hadddir}/{analyzername}_{sample}*Tau*")
+            os.system(f"hadd ../RootFiles/{outdir}/{basename}_{sample}.root {hadddir}/{analyzername}_{sample}*Tau*")
 
     for V in ["W","DY"] :
-        os.system(f"cp {hadddir}/{analyzername}_{V}Jets_MG_TauHLT.root ../RootFiles/{outdir}")
+        if not hasSkim : os.system(f"cp {hadddir}/{analyzername}_{V}Jets_MG_TauHLT.root ../RootFiles/{outdir}")
+        else : os.system(f"cp {hadddir}/{analyzername}_{V}Jets_MG.root ../RootFiles/{outdir}/{basename}_{V}Jets_MG.root")
 
     # Nonprompt HADD
     fullnonprompt = ""
     for np in nonprompts :
-        hadddir = f"{GetSKOutDir(analyzername,era)}/{flagstr}{np}"
+        hadddir = f"{GetSKOutDir(basename,era)}/{flagstr}{np}"
         allfiles = glob.glob(f"{hadddir}/*.root")
         bkgonly = [file for file in allfiles if "WRtoTauNtoTau" not in file]
         haddstr = ""
         for bkgf in bkgonly : haddstr += f"{bkgf} "
-        os.system(f"hadd ../RootFiles/{outdir}/{analyzername}_{np}.root {haddstr}")
-        fullnonprompt += f"../RootFiles/{outdir}/{analyzername}_{np}.root "
+        os.system(f"hadd ../RootFiles/{outdir}/{basename}_{np}.root {haddstr}")
+        fullnonprompt += f"../RootFiles/{outdir}/{basename}_{np}.root "
 
-    os.system(f"hadd ../RootFiles/{outdir}/{analyzername}_Nonprompt.root {fullnonprompt}")
+    os.system(f"hadd ../RootFiles/{outdir}/{basename}_Nonprompt.root {fullnonprompt}")
     
 
 
@@ -70,10 +86,11 @@ def HADDnGet(analyzername,era,flag,outdir) :
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Hadd output files from WRTauAnalyzer')
-    parser.add_argument('--flag', type=str, help='Additional flag used',default='')
-    parser.add_argument('--outdir', type=str, help='Output dir',default=GetTMPDir())
-    parser.add_argument('--era', type=int,  help='Era, if 0 full era combination')
-    parser.add_argument('--analyzername', type=str, help='Analyzer name',default='WRTau_Analyzer')
+    parser.add_argument('--flag'  , type=str , help='Additional flag used' , default = '' ) 
+    parser.add_argument('--outdir', type=str , help='Output dir'           , default = GetTMPDir() )
+    parser.add_argument('--era'   , type=int , help='Era, if 0 full era combination')
+    parser.add_argument('--skim'  , type=str , help='Skim name i.e Name if SkimTree_Name' , default = '' )
+    parser.add_argument('--analyzername', type=str , help='Analyzer name'  , default = 'WRTau_Analyzer' )
     args = parser.parse_args()
 
-    HADDnGet(args.analyzername,args.era,args.flag,args.outdir)
+    HADDnGet(args.analyzername,args.era,args.flag,args.outdir,args.skim)
