1e1b385f07f3aa7ad8be046f5bfa9e8836d93af4
Youngwan Kim <youngwan.kim@cern.ch>
2023-12-21 15:19:35 +0900

    fr

--- Fake/FakeRatePlotter.py
+++ Fake/FakeRatePlotter.py
@@ -7,8 +7,13 @@ savestr = filename.split("_")[1]
 f_fake = TFile(f"{filename}.root")
 
 d_geoTag = {"All" : "#eta Inclusive",
-            "B"   : "#eta Inclusive",
-            "EC"  : "#eta Inclusive"}
+            "B"   : "|#eta| < 1.479",
+            "EC"  : "|#eta| > 1.479"}
+
+d_njtag = {"2" : "N_{AK4} = 2",
+           "3" : "N_{AK4} = 3",
+           "4" : "N_{AK4} # 3"
+}
 
 def drawLatex(region,x1=0.175,y1=0.8,x2=0.575,y2=0.925):
     latex = TLatex()
@@ -34,16 +39,55 @@ def drawLatex(region,x1=0.175,y1=0.8,x2=0.575,y2=0.925):
     elif region == 1  : region_latex = "Resolved #mu#tau_{h} Fake CR"
     elif region == 2  : region_latex = "Inclusive #mu#tau_{h} Fake CR"
     latex.DrawLatex(x2+0.045, y1-0.055,f"{region_latex}")
-    latex.SetTextSize(0.45*textSize)
-    latex.DrawLatex(x2+0.145, y1-0.095,"(#slash{E}_{T} < 100 GeV)")
     latex.SetTextSize(0.65*textSize)
     latex.DrawLatex(x2-0.075, y1+0.015 , "FR(#tau_{h}) = #scale[0.7]{#frac{VVVLoose && Tight}{VVVLoose}}")
+
+def drawTagLatex(eta,nj,x=.575,y=.8) :
+    latex = TLatex()
+    latex.SetNDC()
+    textSize = 0.625*gStyle.GetPadTopMargin()
+        latex.SetTextSize(0.45*textSize)
+    latex.DrawLatex(x+0.145, y1-0.095,"(#slash{E}_{T} < 100 GeV)")
 
 
 c = TCanvas("","",1000,1000)
 ptbins = [0,190,300,400,500,600,1000,2000]
 #ptbins = [0,190,400,2000]
 ptbins = [0,190,250,350,450,600,2000]
+
+for eta in d_geoTag :
+    for nj in d_njtag :
+        for i,r in enumerate(["BoostedSignalRegionMETInvert","ResolvedSignalRegionMETInvert"]) :
+        c = TCanvas("","",1000,1000)
+        h_loose = f_fake.Get(f"WRTauFake/{r}/TauPt_Loose_{eta}_{nj}").Rebin(len(ptbins)-1,f"loose{r}",array.array('d',ptbins))
+        h_tight = f_fake.Get(f"WRTauFake/{r}/TauPt_Tight_{eta}_{nj}").Rebin(len(ptbins)-1,f"tight{r}",array.array('d',ptbins))
+        h_fr = h_tight.Clone(f"{r}_FR")
+        h_fr.Divide(h_loose)
+        h_fr.GetYaxis().SetRangeUser(0,1.75)
+        h_fr.SetStats(0)
+        h_fr.GetXaxis().SetTitle("p_{T}(#tau_{h})")
+        h_fr.GetYaxis().SetTitleSize(0.05)
+        h_fr.GetYaxis().SetTitle("FR(#tau_{h})")
+        h_fr.GetYaxis().SetTitleOffset(0.9)
+        h_fr.GetXaxis().SetNdivisions(509)
+        h_fr.SetLineColor(kRed)
+        h_fr.SetLineWidth(3)
+        h_fr.GetXaxis().SetTitleSize(0.05)
+        h_err = h_fr.Clone(f"{r}_FR_err")
+        h_err.SetLineWidth(2)
+        h_err.SetLineColor(kBlack)
+
+
+        c.cd()
+        c.SetLeftMargin(0.125)
+        c.SetRightMargin(0.085)
+        c.SetBottomMargin(0.125)
+        #h_fr.Draw("hist")
+        h_err.Draw("e0")
+        h_fr.Draw("hist&same")
+        drawLatex(i)
+        c.SaveAs(f"TauFE_{savestr}_{r}.png")
+
 
 for i,r in enumerate(["BoostedSignalRegionMETInvert","ResolvedSignalRegionMETInvert"]) :
     c = TCanvas("","",1000,1000)
