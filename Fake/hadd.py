7b4c9c5a4c89e8b726035d9b7bf6ba6b840fc69e
Youngwan Kim <youngwan.kim@cern.ch>
2024-03-18 13:00:00 +0900

    2018 sample related updates

--- Fake/hadd.py
+++ Fake/hadd.py
@@ -2,9 +2,11 @@ import os
 from datetime import datetime
 
 savestr = datetime.now().strftime('%Y%m%d_%H%M%S')
-os.system(f"mkdir -p Inputs/{savestr}/Split")
-os.system(f"mkdir -p Inputs/{savestr}/Data")
-os.system(f"cp $SKFlatOutputDir/Run2UltraLegacy_v3/WRTau_TauFake/2017/WRTau_TauFake_SkimTree_LRSMTau_* Inputs/{savestr}/Split")
-os.system(f"hadd Inputs/{savestr}/TauFake_{savestr}_QCDOnly.root $SKFlatOutputDir/Run2UltraLegacy_v3/WRTau_TauFake/2017/WRTau_TauFake_SkimTree_LRSMTau_QCD_Pt*")
-os.system(f"hadd Inputs/{savestr}/TauFake_{savestr}.root $SKFlatOutputDir/Run2UltraLegacy_v3/WRTau_TauFake/2017/WRTau_TauFake_SkimTree_LRSMTau_*")
-os.system(f"hadd Inputs/{savestr}/Data/TauFake_{savestr}.root $SKFlatOutputDir/Run2UltraLegacy_v3/WRTau_TauFake/2017/DATA/*")
+
+for era in ["2017","2018"] :
+    os.system(f"mkdir -p Inputs/{savestr}/{era}/Split")
+    os.system(f"mkdir -p Inputs/{savestr}/{era}/Data")
+    os.system(f"cp $SKFlatOutputDir/Run2UltraLegacy_v3/WRTau_TauFake/{era}/WRTau_TauFake_SkimTree_LRSMTau_* Inputs/{savestr}/Split")
+    os.system(f"hadd Inputs/{savestr}/{era}/TauFake_{savestr}_QCDOnly.root $SKFlatOutputDir/Run2UltraLegacy_v3/WRTau_TauFake/2017/WRTau_TauFake_SkimTree_LRSMTau_QCD_Pt*")
+    os.system(f"hadd Inputs/{savestr}/{era}/TauFake_{savestr}.root $SKFlatOutputDir/Run2UltraLegacy_v3/WRTau_TauFake/2017/WRTau_TauFake_SkimTree_LRSMTau_*")
+    os.system(f"hadd Inputs/{savestr}/{era}/Data/TauFake_{savestr}.root $SKFlatOutputDir/Run2UltraLegacy_v3/WRTau_TauFake/2017/DATA/*")
